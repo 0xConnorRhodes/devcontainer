8e08947fe824ab1ebfda8563ec18f954cbe09cd4
Connor Rhodes <connor@rhodes.contact>
2024-05-18 22:04:28 -0500

    switched config deployment to entrypoint script for per-node variation

--- bootstrap/entrypoint.sh
+++ bootstrap/entrypoint.sh
@@ -8,7 +8,7 @@ KEY_PATH="$HOME/.ssh/id_ed25519"
 MACHINE=$(hostname)
 if [ ! -f "$KEY_PATH" ]; then
     ssh-keygen -a 100 -t ed25519 -f $HOME/.ssh/id_ed25519 -C "connor@$MACHINE"
-    echo 'generated new key' >> $HOME/init-log.txt
+    echo 'generated new ssh key' >> $HOME/init-log.txt
 fi
 
 sh -c "tail -f /dev/null"
