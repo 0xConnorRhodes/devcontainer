43e07626ee859c6b53103084f2f24ac881e99e80
Connor Rhodes <connor@rhodes.contact>
2024-05-21 17:26:55 +0000

    added step in entrypoint.sh to mark that startup process has finished

--- bootstrap/entrypoint.sh
+++ bootstrap/entrypoint.sh
@@ -21,7 +21,7 @@ fi
 # update fish shell completions (needec because they are persistent on a bind mount)
 fish -c fish_update_completions
 
-sudo touch /opt/bootstrap/boot-finished
+sudo touch /opt/bootstrap/startup-finished
 
 # infinite task to hold container open so shell sessions can be attached
 sh -c "tail -f /dev/null"
